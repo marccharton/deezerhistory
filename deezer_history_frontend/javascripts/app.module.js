4eedfed440e368ca8a3d1f6cf95e2f232e2fa80a
Marc Charton <kramoogle@gmail.com>
2015-10-12 08:50:51 +0000

    app.module.js créé en ligne via Bitbucket

---
+++ deezer_history_frontend/javascripts/app.module.js
@@ -0,0 +1,18 @@
+// 
+// 
+// @filename app.module.js
+// @author kram47
+// 
+// 
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app',
+        [
+            'ngRoute', 
+            'ngResource'
+        ]);
+
+})();
